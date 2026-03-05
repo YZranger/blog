8a3237c3f3b27a3b7657f9ad30246743ea23c81a
yz <yz@geek.blog>
2026-03-05 16:13:33 +0800

    fix: 改为根路径部署

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
-  base: '/blog/',
+  base: '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets'
